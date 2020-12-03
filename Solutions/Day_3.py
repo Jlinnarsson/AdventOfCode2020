cfc05097dcd4bc0d1876fd1abd05fcc291a2628d
jesper.linnarsson <jesper.linnarsson@gmail.com>
2020-12-03 20:58:37 +0100

    Added solution for day 3

--- Solutions/Day_3.py
+++ Solutions/Day_3.py
@@ -1,13 +1,30 @@
-
-
-
-
 def ReadInput():
     with open('./InputData/Day_3.txt') as reader:
-        for line in reader:
-            input_values = []
-            input_values.append(line.split())
-        return input_values
+        return reader.read().splitlines()
 
+def FindTrees(map, xStep, yStep):
+    xPos = 0
+    yPos = 0
+    trees = 0
+    while yPos < len(map):
+        if xPos > len(map[yPos])-1:
+            xPos = xPos - len(map[yPos])
+        if map[yPos][xPos] == '#':
+            trees += 1
+        xPos += xStep
+        yPos += yStep
+    return trees
+        
 data = ReadInput()
-print(data)
+#part 1
+print("Part 1")
+print(FindTrees(data, 3, 1))
+
+#part 2
+slope1 = FindTrees(data, 1, 1)
+slope2 = FindTrees(data, 3, 1)
+slope3 = FindTrees(data, 5, 1)
+slope4 = FindTrees(data, 7, 1)
+slope5 = FindTrees(data, 1, 2)
+print("Part 2")
+print(slope1*slope2*slope3*slope4*slope5)
